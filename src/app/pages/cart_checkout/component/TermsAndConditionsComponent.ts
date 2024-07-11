8c363d2e58e33267cb0981e08e65b27e81ddae10
Eugene Petrik <eugeniy.petrik@gmail.com>
2024-07-11 10:23:22 +0300

    Fix typo

--- src/app/pages/cart_checkout/component/TermsAndConditionsComponent.ts
+++ src/app/pages/cart_checkout/component/TermsAndConditionsComponent.ts
@@ -3,10 +3,10 @@ import { Component } from '../../../abstract.classes';
 import { step } from '../../../../utils/reporters/steps';
 
 export class TermsAndConditions extends Component {
-  private readonly ivereadCheckbox = this.page.locator('#accepttos_custom');
+  private readonly iHaveReadCheckbox = this.page.locator('#accepttos_custom');
 
   @step()
   async expectLoaded(): Promise<void> {
-    await Promise.all([await expect(this.ivereadCheckbox).toBeVisible()]);
+    await Promise.all([await expect(this.iHaveReadCheckbox).toBeVisible()]);
   }
 }
