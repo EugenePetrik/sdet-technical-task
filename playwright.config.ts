a947431cb079ee91fd027b48aeda1518699503f7
Eugene Petrik <eugeniy.petrik@gmail.com>
2025-02-02 18:36:48 +0200

    Format config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,55 +7,39 @@ config();
 
 export default defineConfig({
   testDir: join(process.cwd(), 'src', 'tests'),
-
   fullyParallel: true,
-
   forbidOnly: !!process.env.CI,
-
   maxFailures: process.env.CI ? 5 : 10,
-
   retries: process.env.CI ? 1 : 0,
-
   workers: process.env.CI ? 2 : 1,
-
   reporter: [
     ['list', { printSteps: false }],
     ['html', { open: 'never' }],
     [join(process.cwd(), 'src', 'utils', 'reporters', 'CustomReporterConfig.ts')],
   ],
-
   timeout: 60_000,
-
   expect: {
     timeout: 10_000,
   },
-
   use: {
     headless: !!process.env.CI,
-
     ignoreHTTPSErrors: true,
-
     actionTimeout: 15_000,
-
     baseURL: CONFIG.BASE_URL,
-
     trace: {
       mode: 'retain-on-failure',
     },
-
     screenshot: {
       fullPage: true,
       mode: 'only-on-failure',
     },
   },
-
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
       grep: [new RegExp('@e2e')],
     },
-
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
